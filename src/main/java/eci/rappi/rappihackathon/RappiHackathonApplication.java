5492e7c3e9cea1a81cfe8a9b2a4ce0ec003d14d7
Alejandro Anzola Avila <anzola95avila@gmail.com>
2018-09-15 12:27:08 -0500

    proyecto inicial

---
+++ src/main/java/eci/rappi/rappihackathon/RappiHackathonApplication.java
@@ -0,0 +1,12 @@
+package eci.rappi.rappihackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RappiHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RappiHackathonApplication.class, args);
+	}
+}
---
+++ src/test/java/eci/rappi/rappihackathon/RappiHackathonApplicationTests.java
@@ -0,0 +1,16 @@
+package eci.rappi.rappihackathon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RappiHackathonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
