1739e76da4e12a786a4d9504c891c7d57aea240b
Diego Borrero <die.9511@gmail.com>
2018-09-15 15:37:11 -0500

    Test conection posgres

--- src/test/java/eci/rappi/rappihackathon/RappiHackathonApplicationTests.java
+++ src/test/java/eci/rappi/rappihackathon/RappiHackathonApplicationTests.java
@@ -6,6 +6,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RappiHackathonApplicationTests {
@@ -19,6 +22,11 @@ public class RappiHackathonApplicationTests {
 		System.out.println("Test de prueba");
 
 		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void testConectionPosgres(){
+		Assert.assertNotNull(AppConfiguration.CreateConectionPostgres());
 	}
 
 }
